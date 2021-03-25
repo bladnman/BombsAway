663cbf718f0de156eab9c6277f943e6a0d60c500
Matt Maher <matt@maher-family.net>
2021-03-25 07:08:56 -0700

    pre-unit test

---
+++ BombsAway/objects/player/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  BombsAway
+//
+//  Created by Maher, Matt on 3/24/21.
+//
+
+import SceneKit
+
+class Player {
+  var name: String?
+  var isCPU = false
+  var playerShip: SCNNode?
+  var offenseBoard: Board?
+  var defenseBoard: Board?
+}
